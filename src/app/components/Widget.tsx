b9e03feb6a9770f5f86253c1e671c930c490c109
Parth Marfatia <parthmarfatia153@gmail.com>
2024-06-21 04:01:29 +0530

    animation

--- src/app/components/Widget.tsx
+++ src/app/components/Widget.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Widget = () => {
   return (
-    <div className="invisible md:visible bg-dark-gray/50 fixed right-10 bottom-10 flex flex-col gap-4 py-4 px-2 rounded-full">
+    <div className="invisible md:visible bg-dark-gray/50 fixed right-10 bottom-10 flex flex-col gap-4 py-4 px-2 rounded-full animate-fade-in">
       <span className="material-icons text-white cursor-pointer">facebook</span>
       <span className="material-icons text-white cursor-pointer">email</span>
       <span className="material-icons text-white cursor-pointer">phone</span>
