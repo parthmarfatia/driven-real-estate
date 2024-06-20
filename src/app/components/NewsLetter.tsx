9903135454b8273e6855d353fb0b115962f7a5d0
Parth Marfatia <parthmarfatia153@gmail.com>
2024-06-20 15:54:35 +0530

    newsletter section

--- src/app/components/NewsLetter.tsx
+++ src/app/components/NewsLetter.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const NewsLetter = () => {
-  return <div>NewsLetter</div>;
+  return (
+    <section className="newsletter py-16 h-full w-full flex flex-col items-center gap-8 bg-offwhite">
+      <div className="w-[55rem] text-center">
+        <h2>Get the Real Estate Insights Newsletter in Your Inbox Monthly</h2>
+      </div>
+      <div className="flex gap-6">
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="pl-8 w-96 font-bold rounded border border-midgray placeholder:text-midgray"
+        />
+        <button className="rounded bg-elegant-blue py-4 px-8 text-white font-bold border border-elegant-blue hover:bg-opacity-0 hover:border hover:border-elegant-blue hover:text-elegant-blue">
+          Sign Up
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default NewsLetter;
