3f2eb81b205e59efbd395e4bd516d45078beccad
Parth Marfatia <parthmarfatia153@gmail.com>
2024-06-21 11:43:34 +0530

    z index fix widget

--- src/app/components/Widget.tsx
+++ src/app/components/Widget.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Widget = () => {
   return (
-    <div className="invisible cursor-pointer md:visible bg-dark-gray/50 fixed right-10 bottom-10 flex flex-col gap-4 py-4 px-2 rounded-full ">
+    <div className="invisible z-50 cursor-pointer md:visible bg-dark-gray/50 fixed right-10 bottom-10 flex flex-col gap-4 py-4 px-2 rounded-full ">
       <span className="material-icons text-white hover:text-optienal-dark-blue cursor-pointer">
         facebook
       </span>
